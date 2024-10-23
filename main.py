c367fe8b3b22c215ad1f087b84f79ce288c036f9
TAMOTO24 <sanicyll@gmail.com>
2024-10-23 06:06:10 +0300

    some scale update

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 import pygame
 from modules.structureClasses import MenuController
+from modules.structureClasses import ManualObjects
 
 pygame.init()
 SIZEH, SIZEW = 920, 1360
 
-mc = MenuController()
+min_width, min_height = 500, 800
+
+MC = MenuController()
 screen = pygame.display.set_mode([SIZEW, SIZEH], pygame.RESIZABLE, vsync=1)  # Screen properties
 pygame.display.set_caption('BEASTARS')
 
 menuBG = pygame.image.load(r'assets/Background/Preview1.png')
-menuBG = mc.structureObjSize(menuBG, SIZEW, SIZEH)
+menuBG_obj = MC.structureBGSize(menuBG, SIZEW, SIZEH)
+
+MO = ManualObjects()
+startBtn = MO.button(50, "START", r'assets/Font/Fonstars-4Bo0p.otf')
 
 running = True
 while running:
@@ -18,11 +24,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.VIDEORESIZE:
-            menuBG = mc.structureObjSize(menuBG, event.w, event.h)
+            menuBG_obj = MC.structureBGSize(menuBG, event.w, event.h)
+
+            # if event.w < min_width or event.h < min_height:
+            #     width = max(event.w, min_width)
+            #     height = max(event.h, min_height)
+            #     screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
     CURRENTW, CURRENTH = pygame.display.get_window_size()
 
-    screen = mc.setCentre(menuBG, CURRENTW, CURRENTH, screen)
+    screen = MC.setCentre(menuBG_obj, CURRENTW, CURRENTH, screen)
+
+    startBtn['surf'].blit(startBtn['text'], startBtn['textRect']) #set text in btn surface
+    screen = MC.setObjCoordIncludeScale(screen, CURRENTW, CURRENTH, startBtn['surf'], 90, 10)
 
     pygame.display.flip()
 
--- modules/structureClasses.py
+++ modules/structureClasses.py
@@ -4,22 +4,54 @@ class MenuController:
     def __init__(self):
         pygame.init()
 
-    def structureObjSize(self, obj, width, height): #Scale object to screen size
+    def structureBGSize(self, obj, width, height): #Scale object to screen size
 
         original_width, original_height = obj.get_size()
 
         scale_factor = min(width / original_width, height / original_height)
 
-        new_width = int(original_width * scale_factor)
-        new_height = int(original_height * scale_factor)
+        self.new_width = int(original_width * scale_factor)
+        self.new_height = int(original_height * scale_factor)
 
-        obj = pygame.transform.scale(obj, (new_width, new_height))
+        obj = pygame.transform.scale(obj, (self.new_width, self.new_height))
 
         return obj
-    def setCentre(self, obj, width, height, screen, moveX=0, moveY=0):
+    def setCentre(self, obj, width, height, screen):
 
         obj_width, obj_height = obj.get_size()
         
-        screen.blit(obj, (((width - obj_width) // 2) + moveX, ((height - obj_height) // 2) + moveY))
+        screen.blit(obj, (((width - obj_width) // 2), ((height - obj_height) // 2)))
 
         return screen
+    def setObjCoordIncludeScale(self, screen, width, height, obj, moveX=0, moveY=0):
+        x = width - self.new_width
+        y = height - self.new_height
+
+        if moveX > 0:
+            moveX =  int(self.new_width * moveX//100) # procent of width
+        if moveY > 0:
+            moveY =  int(self.new_height * moveY//100) # procent of height
+
+        screen.blit(obj, (((x//2) + moveX) - obj.get_width(), ((y//2) + moveY) - obj.get_height())) # set coord 0,0 including screen scale (and add user X, Y to move on screen)
+
+        return screen
+
+class ManualObjects:
+    def __init__(self):
+        pygame.init()
+    def button(self, TXTsize, text="button text", fontPath="Arial"):
+        font = pygame.font.Font(fontPath, TXTsize)
+        text = font.render(text, 1, (0, 0, 0))
+        
+        button_surface = pygame.Surface((200, 50), pygame.SRCALPHA)
+
+        text_rect = text.get_rect(
+            center=(button_surface.get_width() /2, 
+            button_surface.get_height()/2))
+
+        button_rect = pygame.Rect(125, 125, 150, 50)
+
+        return {'text': text,'textRect' : text_rect, 'btn' : button_rect, 'surf' : button_surface}
+
+
+    
